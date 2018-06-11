01c41927a442f990585f68d39ab5b213a07f3726
bulkasamurai <bulkasamurai@gmail.com>
2018-06-12 01:00:47 +0300

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -169,9 +169,9 @@
      * freshest data.
      */
     app.getForecast = function(key, label) {
-        var statement = 'select * from weather.forecast where woeid=' + key;
+        var statement = "select * from weather.forecast where woeid=" + key + " and u='c'";
         var url = 'https://query.yahooapis.com/v1/public/yql?format=json&q=' +
-            statement + 'and u="c"';
+            statement;
         // TODO add cache logic here
         if ('caches' in window) {
             /*
