2036831bc7bb4d058fe439e684c25495fa856fe1
bulkasamurai <bulkasamurai@gmail.com>
2018-06-12 21:12:56 +0300

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -16,10 +16,6 @@
 (function() {
     'use strict';
 
-    /*if (navigator.serviceWorker) {
-        navigator.serviceWorker.register('/bulkasamurai/sw.js', {scope: '/bulkasamurai/'})
-    }*/
-
     var app = {
         isLoading: true,
         visibleCards: {},
@@ -37,7 +33,6 @@
      * Event listeners for UI elements
      *
      ****************************************************************************/
-
     document.getElementById('butRefresh').addEventListener('click', function() {
         // Refresh all of the forecasts
         app.updateForecasts();
@@ -297,42 +292,43 @@
      * discussion.
      */
     var initialWeatherForecast = {
-        key: '2459115',
-        label: 'New York, NY',
-        created: '2016-07-22T01:00:00Z',
+        key: '834463',
+        label: 'Minsk',
+        created: '',
         channel: {
             astronomy: {
-                sunrise: "5:43 am",
-                sunset: "8:21 pm"
+                sunrise: "",
+                sunset: ""
             },
             item: {
                 condition: {
-                    text: "Windy",
-                    date: "Thu, 21 Jul 2016 09:00 PM EDT",
-                    temp: 56,
-                    code: 24
+                    text: "",
+                    date: new Date(),
+                    temp: null,
+                    code: null
                 },
                 forecast: [
-                    {code: 44, high: 86, low: 70},
-                    {code: 44, high: 94, low: 73},
-                    {code: 4, high: 95, low: 78},
-                    {code: 24, high: 75, low: 89},
-                    {code: 24, high: 89, low: 77},
-                    {code: 44, high: 92, low: 79},
-                    {code: 44, high: 89, low: 77}
+                    {code: null, high: null, low: null},
+                    {code: null, high: null, low: null},
+                    {code: null, high: null, low: null},
+                    {code: null, high: null, low: null},
+                    {code: null, high: null, low: null},
+                    {code: null, high: null, low: null},
+                    {code: null, high: null, low: null}
                 ]
             },
             atmosphere: {
-                humidity: 56
+                humidity: null
             },
             wind: {
-                speed: 25,
-                direction: 195
+                speed: null,
+                direction: null
             }
         }
     };
     // TODO uncomment line below to test app with fake data
     app.updateForecastCard(initialWeatherForecast);
+    app.updateForecasts();
 
     /************************************************************************
      *
